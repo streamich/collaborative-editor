e52e6fd05a507037559a0ea91fd6784d20a128e0
streamich <streamich@gmail.com>
2024-07-24 22:49:53 +0200

    test: 💍 fix tests after latest changes

--- src/__tests__/StrBinding.spec.ts
+++ src/__tests__/StrBinding.spec.ts
@@ -4,9 +4,9 @@ import {MemoryEditor0, MemoryEditor1} from '../MemoryEditor';
 
 describe('.set()', () => {
   test('syncs model to editor on init', () => {
-    const model = Model.withLogicalClock();
+    const model = Model.create();
     model.api.root({str: 'start'});
-    const str = model.api.str(['str']);
+    const str = () => model.api.str(['str']);
     const editor = new MemoryEditor0();
     expect(editor.__str).toBe('');
     const unbind = StrBinding.bind(str, editor);
@@ -17,9 +17,9 @@ describe('.set()', () => {
 
 describe('.ins() and .del()', () => {
   test('syncs model to editor on startup using granular methods', () => {
-    const model = Model.withLogicalClock();
+    const model = Model.create();
     model.api.root({str: 'start'});
-    const str = model.api.str(['str']);
+    const str = () => model.api.str(['str']);
     const editor = new MemoryEditor1();
     expect(editor.__str).toBe('');
     const unbind = StrBinding.bind(str, editor);
@@ -28,9 +28,9 @@ describe('.ins() and .del()', () => {
   });
 
   test('syncs model to editor on startup using granular methods', () => {
-    const model = Model.withLogicalClock();
+    const model = Model.create();
     model.api.root({str: 'abc def ghi'});
-    const str = model.api.str(['str']);
+    const str = () => model.api.str(['str']);
     const editor = new MemoryEditor1();
     editor.__str = 'abc edf x';
     const unbind = StrBinding.bind(str, editor);
